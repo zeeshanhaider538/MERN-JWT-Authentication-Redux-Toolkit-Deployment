9b1a8f3e85878223499c2965f4e3435ac5f1a593
zeeshanhaiderte <zeeshanhaider.te@gmail.com>
2024-03-27 11:01:55 +0500

    Authorization done as protected routes

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -51,14 +51,24 @@ const resgisterUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "User Logged Out" });
 });
 
 // @desc    Get user Profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
 });
 
 // @desc    Update User Profile
--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,8 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import connectDB from "./config/db.js";
 
+import cookieParser from "cookie-parser";
+
 // Now you can access configuration variables using process.env
 const port = process.env.PORT || 5000;
 
@@ -23,6 +25,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 ///
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("<h1>Server is ready</h1>"));
